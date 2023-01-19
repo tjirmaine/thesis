3806f36ff762d34834f956cc9c628559ee3236b4
tjirmaine <tjirmaine@gmail.com>
2023-01-19 18:00:22 +0100

    Now runs the first experiment, d<3 and sim>T

--- main/Main.py
+++ main/Main.py
@@ -1,51 +1,100 @@
-def adoption(graph, adopted, threshold):
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+
+def adoption(graph, adoption, threshold):
     """Function for updating adopted nodes in a graph"""
-    # new adopters list so that it doesn't effect calculations
-    new_adopters = []
 
-    # for non monotonic
-    #new_nonadopters = []
+    new_adoption = {}
 
-    # loop all nodes
-    for node in graph:
+    for behaviour, adopted in adoption.items():
+        # new adopters list so that it doesn't effect calculations
+        new_adopters = []
 
-        ### MONOTONIC ###
-        # if node already adopted then skip
-        if node in adopted:
-            continue
+        # for non monotonic
+        #new_nonadopters = []
 
-        # initialise count and get node's neighbours
-        count = 0
+        # loop all nodes
+        for node in graph:
 
-        neighbours = graph[node]
+            ### MONOTONIC ###
+            # if node already adopted then skip
+            if node in adopted:
+                continue
 
-        # loop over all neighbours and increment count if that neighbour is an adopter
-        for neighbour in neighbours:
-            if neighbour in adopted:
-                count += 1
+            # initialise count and get node's neighbours
+            count = 0
 
-        # calculate ratio adopters/neighbours
-        if len(neighbours) == 0:
-            ratio = 0
-        else:
-            ratio = count/len(neighbours)
+            neighbours = graph[node]
 
-        #### MONOTONIC ####
-        # add node to new adopters if ratio is geq than threshold
-        if ratio >= threshold:
-            new_adopters.append(node)
+            # loop over all neighbours and increment count if that neighbour is an adopter
+            for neighbour in neighbours:
+                if neighbour in adopted:
+                    count += 1
 
-        #### NON-MONOTONIC ####
-        # remove node from adopters if ratio is less than threshold
-        # elif ratio < threshold:
-        #     adopted.remove(node)
-    for new_adopter in new_adopters:
-        adopted.append(new_adopter)
+            # calculate ratio adopters/neighbours
+            if len(neighbours) == 0:
+                ratio = 0
+            else:
+                ratio = count/len(neighbours)
+
+            #### MONOTONIC ####
+            # add node to new adopters if ratio is geq than threshold
+            if ratio >= threshold:
+                new_adopters.append(node)
+
+            #### NON-MONOTONIC ####
+            # remove node from adopters if ratio is less than threshold
+            # elif ratio < threshold:
+            #     adopted.remove(node)
+        new_adoption[behaviour] = new_adopters
+
+    return new_adoption
+    # for new_adopter in new_adopters:
+    #     adopted.append(new_adopter)
 
 
 def full_cascade(adopted, n_nodes):
     """Function to check whether a full cascade has occurred"""
-    return True if n_nodes == len(adopted) else False
+    full_cascade = []
+    for behaviour, nodes in adopted.items():
+        if len(nodes) == n_nodes:
+            full_cascade.append(behaviour)
+    return full_cascade
+
+
+def stable_state(new_edges, adopters):
+    """Function to check whether graph has reached a stable state"""
+    for node, edges in new_edges.items():
+        # if new_edges dictionary has a non-empty array then not stable
+        if edges:
+            return False
+    for behaviour, nodes in adopters.items():
+        # if new_adopters dictionary has a non-empty array then not stable
+        if nodes:
+            return False
+
+    # both dictionaries were empty, therefore stable
+    return True
+
+
+def similarity(n1, n2, adopters):
+    """Function to check the similarity between two nodes"""
+    sim = []
+
+    # loop over all behaviour sets
+    for behaviour, adopted in adopters.items():
+
+        # if both nodes are adopters add the behaviour
+        if n1 in adopted and n2 in adopted:
+            sim.append(behaviour)
+        # else if both nodes are not adopters add the behaviour
+        elif n1 not in adopted and n2 not in adopted:
+            sim.append(behaviour)
+
+    # return list of similar behaviours
+    return sim
 
 
 def merge(links, new_links):
@@ -58,7 +107,7 @@ def merge(links, new_links):
             links[node].append(new_neighbour)
 
 
-def friendship_selection(graph):
+def friendship_selection(graph, adopters, threshold):
     """Function for updating edges in graph"""
 
     # making temp graph
@@ -83,11 +132,23 @@ def friendship_selection(graph):
                 # ignore itself
                 if node == second_neighbour:
                     continue
-                if second_neighbour not in neighbours and second_neighbour not in new_neighbours:
-                    add_neighbour(node, second_neighbour, new_edges)
-                    # print(f'new neighbours')
 
-    merge(graph, new_edges)
+                # potential new neighbour
+                if second_neighbour not in neighbours and second_neighbour not in new_neighbours:
+                    # check if node similarities
+                    sim = similarity(node, second_neighbour, adopters)
+                    # print(sim)
+
+                    # sanity check
+                    if len(adopters) > 0:
+                        ratio = len(sim) / len(adopters)
+
+                        # add link if ratio exceeds threshold
+                        if ratio > threshold:
+                            add_neighbour(node, second_neighbour, new_edges)
+                    else:
+                        continue
+    return new_edges
 
 
 def add_neighbour(n1, n2, graph):
@@ -107,37 +168,69 @@ by Jirmaine Tan
 This program is able to perform graph network updates
 """
 if __name__ == "__main__":
-    # initialise number of nodes, list of nodes, threshold, edges, adopters and behaviours
-    n_nodes = 4
-    nodes = []
-    threshold = 0.3
+    # declare number of runs (how many random networks)
+    runs = 50
+
+    # initialise social networks model
+    n_nodes = 10
+    p_edge = 0.3
+    p_behaviour = 0.3
+    threshold_adopt = 0.3
+    threshold_link = 0.3
     links = {}
     adopters = {}
-    behaviours = ['A']
+    behaviours = ['A', 'B', 'C', 'D']
 
-    # add nodes to graph
-    for i in range(n_nodes):
-        nodes.append(i)
-        add_node(i, links)
+    for n in range(runs):
+        # generate random graph (erdos-renyi) with n-nodes and probability p
+        graph = nx.erdos_renyi_graph(n_nodes, p_edge)
+        # nx.draw(graph, with_labels=True)
+        # plt.show()
 
-    # starting edges
-    add_neighbour(0, 1, links)
-    add_neighbour(1, 2, links)
-    add_neighbour(2, 3, links)
+        # retrieve edges and nodes
+        edges = graph.edges
+        nodes = graph.nodes
 
-    # initial adopters
-    # 0 is an initial adopter of behaviour 'A'
-    adopters[behaviours[0]] = []
-    adopters[behaviours[0]].append(0)
+        # transfer to adjacency list
+        # add nodes to graph
+        for i in nodes:
+            add_node(i, links)
+        for edge in edges:
+            add_neighbour(edge[0], edge[1], links)
 
-    ### define steps here ###
+        # initial adopters
+        for behaviour in behaviours:
+            adopters[behaviour] = []
+            for node in nodes:
+                rand = random.uniform(0.0, 1.0)
+                if rand <= p_behaviour:
+                    adopters[behaviour].append(node)
 
-    # this is
-    cascade = False
-    counter = 0
-    while not cascade:
-        counter += 1
-        print(counter)
-        friendship_selection(links)
-        adoption(links, adopters[behaviours[0]], threshold)
-        cascade = full_cascade(adopters[behaviours[0]], n_nodes)
+        print(f'RUN: {n}')
+
+        ### define steps here ###
+        # cascade = False
+        stable = False
+        counter = 0
+        while not stable:
+
+            # print network
+            print(f'step: {counter}')
+            print(links)
+            print(adopters)
+
+            # increment counter
+            counter += 1
+
+            # step updates
+            new_edges = friendship_selection(links, adopters, threshold_link)
+            new_adoption = adoption(links, adopters, threshold_adopt)
+
+            stable = stable_state(new_edges, new_adoption)
+            merge(links, new_edges)
+            merge(adopters, new_adoption)
+
+        print(f'stable?: {stable}')
+        print(f'full cascades: {full_cascade(adopters, n_nodes)}')
+
+        runs -= 1
